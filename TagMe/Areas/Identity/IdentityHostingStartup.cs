a38adfb264394ab1cc44007f4730bfd54abe833c
Daryl <43867408+dellod@users.noreply.github.com>
2020-02-27 12:00:16 -0700

    Added identity user stuff

---
+++ TagMe/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TagMe.Data;
+
+[assembly: HostingStartup(typeof(TagMe.Areas.Identity.IdentityHostingStartup))]
+namespace TagMe.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<TagMeContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("TagMeContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<TagMeContext>();
+            });
+        }
+    }
+}
